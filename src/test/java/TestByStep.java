234563d4b06ff1c99ac3bfe9e56a34fa624f8321
I.Maksimov <bif11@mail.ru>
2023-10-11 20:18:14 +0300

    refactor import

--- src/test/java/TestByStep.java
+++ src/test/java/TestByStep.java
@@ -1,16 +1,9 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.StepSearch;
-
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static org.openqa.selenium.By.linkText;
-
 
 public class TestByStep {
 
@@ -21,7 +14,7 @@ public class TestByStep {
     @Link(value = "Abr", url = "ya.ru")
     @DisplayName("Отображение страницы для неавторизованного пользователя")
     @Test
-    public void  searchTest(){
+    public void searchTest() {
         StepSearch steps = new StepSearch();
 
         SelenideLogger.addListener("allure", new AllureSelenide());
